5dd327ee61bed2e61a78c0fc3c89130aeb97966a
Edivaldo Moreira <37005158+EdivaldoMoriera@users.noreply.github.com>
2022-04-28 08:15:04 -0300

    criando tabela

--- src/app/courses/courses.module.ts
+++ src/app/courses/courses.module.ts
@@ -1,8 +1,12 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatTableModule } from '@angular/material/table';
 
 import { CoursesRoutingModule } from './courses-routing.module';
 import { CoursesComponent } from './courses/courses.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+
 
 
 @NgModule({
@@ -11,7 +15,10 @@ import { CoursesComponent } from './courses/courses.component';
   ],
   imports: [
     CommonModule,
-    CoursesRoutingModule
+    CoursesRoutingModule,
+    MatTableModule,
+    MatCardModule,
+    MatToolbarModule
   ]
 })
 export class CoursesModule { }
--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,13 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Course } from '../model/course';
+
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
   styleUrls: ['./courses.component.scss']
 })
 export class CoursesComponent implements OnInit {
+  course:Course[] = [
+    {_id: '1' , name:'Angular', category: 'font-end'},
+    { _id: '2', name: 'Spring', category: 'backend'},
+    {_id: '3' , name:'React', category: 'font-end'},
+    { _id: '4', name: 'Nodejs', category: 'backend'},
+    {_id: '5' , name:'Vue', category: 'font-end'},
+    { _id: '6', name: 'JavaScript', category: 'backend'}
+  ];
+  displayedColumns = ['name' , 'category'];
 
-  constructor() { }
+  constructor() {
+
+   }
 
   ngOnInit(): void {
   }
